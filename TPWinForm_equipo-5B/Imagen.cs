3ad1bf370629112f38f9e6eda7a626b48c8e17a9
Matias Peterka <matias.tauro@hotmail.com>
2025-04-19 21:55:57 -0300

    Agrego Clases Principales

---
+++ TPWinForm_equipo-5B/Imagen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_5B
+{
+    internal class Imagen
+    {
+        public int Id { get; set; }
+        public string Ruta { get; set; }
+        public int ArticuloId { get; set; }
+    }
+}
